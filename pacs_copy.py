4e1f0f9341682a1f598bd972e2aa47be7b220ad4
cesar matos <cesar.matos@gmail.com>
2017-02-01 14:51:56 +0100

    Added logging

--- pacs_copy.py
+++ pacs_copy.py
@@ -7,9 +7,12 @@ from dicom.UID import ExplicitVRLittleEndian, ImplicitVRLittleEndian, \
 import tempfile
 import signal
 import sys
-import pandas as pd
 from os import path
 from datetime import datetime
+import logging
+from logging.config import fileConfig
+
+
 
 # parse commandline
 parser = argparse.ArgumentParser(description='storage SCU example')
@@ -23,15 +26,20 @@ parser.add_argument('-i','--implicit', action='store_true',
                     help='negociate implicit transfer syntax only',
                     default=False)
 parser.add_argument('-o','--output', help='output folder', default=tempfile.gettempdir())
-parser.add_argument('-C','--csv', help='csv file with already processed dicoms', default='dicoms_processed.csv')
+parser.add_argument('-l','--log', help='configuration log file', default='logging.ini')
+parser.add_argument('-C','--csv', help='csv file with the already processed dicoms', default='dicoms_processed.csv')
 parser.add_argument('-e','--explicit', action='store_true',
                     help='negociate explicit transfer syntax only',
                     default=False)
 
 args = parser.parse_args()
 
-# this si called aet in the python library, but we are keeping Thanhs script name
-ouput = "../out/"
+if path.isfile(args.log):
+    fileConfig(args.log)
+else:
+    logging.warning("could not find configuration log file '%s'" % args.log)
+
+logger = logging.getLogger()
 
 if args.implicit:
     ts = [ImplicitVRLittleEndian]
@@ -45,42 +53,43 @@ else:
     ]
 
 def OnAssociateResponse(association):
-    print "Association response received"
+    logger.debug("Association response received")
 
 
 def OnAssociateRequest(association):
-    print "Association resquested"
+    logger.debug("Association resquested")
     return True
 
 def OnReceiveStore(SOPClass, ds):
-    print "Received SOPInstanceUID", ds.SOPInstanceUID
     # do something with dataset. For instance, store it.
+    logger.debug("Received C-STORE SeriesInstanceUID:'%s', SOPInstanceUID:'%s''" 
+                % (ds.SeriesInstanceUID, ds.SOPInstanceUID))
     file_meta = Dataset()
     file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.1.1.2'
     # !! Need valid UID here
     file_meta.MediaStorageSOPInstanceUID = "1.2.3"
     # !!! Need valid UIDs here
-    file_meta.ImplementationClassUID = "1.2.3.4"
+    file_meta.ImplementationClassUID = "1.2.3.4"    
     filename = '%s/%s.dcm' % (args.output, ds.SOPInstanceUID)
     fileds = FileDataset(filename, {},
                      file_meta=file_meta, preamble="\0" * 128)
     fileds.update(ds)
-    #fileds.is_little_endian = True
-    #fileds.is_implicit_VR = True
     fileds.save_as(filename)
-    print "File %s written" % filename
+    logger.info("file %s written" % filename)
     # must return appropriate status
     return SOPClass.Success
 
 # create application entity
-MyAE = AE(args.aet, args.port, [PatientRootFindSOPClass,
-                             PatientRootMoveSOPClass,
+MyAE = AE(args.aet, args.port, [StudyRootFindSOPClass,
+                             StudyRootMoveSOPClass,
                              VerificationSOPClass], [StorageSOPClass], ts)
 
+logger.debug("creating application entity")
 MyAE.OnAssociateResponse = OnAssociateResponse
 MyAE.OnAssociateRequest = OnAssociateRequest
 MyAE.OnReceiveStore = OnReceiveStore
 MyAE.start()
+
 
 # this is to force a quit on ctrl-c (sigint)
 # the extra thread created to receive dicoms is not killed automatically
@@ -92,54 +101,62 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 # remote application entity
+logger.debug("creating remote application entity")
 RemoteAE = dict(Address=args.remotehost, Port=args.remoteport, AET=args.aec)
 
 def list_pacs(dataset):    
     assoc = MyAE.RequestAssociation(RemoteAE)
-    st = assoc.PatientRootFindSOPClass.SCU(dataset, 1)
+    st = assoc.StudyRootFindSOPClass.SCU(dataset, 1)
     items = [ss[1] for ss in st if ss[1]]
     assoc.Release(0)
     return items
 
 def copy_dicom(dataset):    
     assoc = MyAE.RequestAssociation(RemoteAE)
-    gen = assoc.PatientRootMoveSOPClass.SCU(dataset, args.aem, 1)
+    gen = assoc.StudyRootMoveSOPClass.SCU(dataset, args.aem, 1)
     for gg in gen:
-        print "gg!", gg
+        # we have to access the item to copy it (it will be done asynchornously)
+        logger.debug("copying %s" % gg)
+        x = gg
     assoc.Release(0)
 
 # create association with remote AE
-print "Requesting association"
+logger.info("Requesting association")
 assoc = MyAE.RequestAssociation(RemoteAE)
 # perform a DICOM ECHO
-print "DICOM Echo ... ",
 st = assoc.VerificationSOPClass.SCU(1)
-print 'done with status "%s"' % st
+logger.info('done with status "%s"' % st)
 assoc.Release(0)
 
 d = Dataset()
-d.QueryRetrieveLevel = "PATIENT"
-d.SOPInstanceUID = '*'
+d.QueryRetrieveLevel = "STUDY"
+d.SeriesInstanceUID = '*'
+
+processed = dict()
+if path.isfile(args.csv):
+    # opens the csv file with the already processed items
+    with open(args.csv, "r") as f:
+        # skips the header
+        for line in f.readlines()[1:]:
+            items = line.split(',')
+            processed[items[0]]=items[1]
+else:
+    logger.warning('file "%s" with already processed dicoms not found, creating an empty one' % args.csv)
+    # starts a new file
+    with open(args.csv, "w") as f:
+        f.write("SeriesInstanceUID,processed_date\n")
 
 items = list_pacs(d)
-
-# opens the csv file with the already processed items
-if path.isfile(args.csv):
-    processed = pd.read_csv(args.csv, index_col=0)
-else:
-    processed = pd.DataFrame(columns=['processed_date'])
-    processed.to_csv(args.csv)
-
 # will update the file by append the new lines 
 with open(args.csv, 'a') as csv_file:
     for i in items:
-        if i.SOPInstanceUID in processed.index:
-            print "Already processed ", i.SOPInstanceUID
+        if i.SeriesInstanceUID in processed:
+            logger.info("Series already processed SeriesInstanceUID:" + i.SeriesInstanceUID)
         else:
-            print "found new item, copying"
-            print i
+            logger.info("found new series starting download")
+            logger.info(i)
             # writes a new line in the csv file so that it remembers the next time
-            csv_file.write("%s,%s\n" % (i.SOPInstanceUID, datetime.now()))
+            csv_file.write("%s,%s\n" % (i.SeriesInstanceUID, datetime.now()))
             copy_dicom(i)
  
 # done
